445ab7c46867ea5c99c1be762d0ff13f967d44dc
OPBrokenaf <157821524+OPBrokenaf@users.noreply.github.com>
2024-04-18 15:34:50 -0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvulO7FD2dwyV-1cAiaqf-Iq3SrbiO78k",
+  authDomain: "booksr-us-9f1e6.firebaseapp.com",
+  projectId: "booksr-us-9f1e6",
+  storageBucket: "booksr-us-9f1e6.appspot.com",
+  messagingSenderId: "853405259540",
+  appId: "1:853405259540:web:57dad89954e22014661401",
+  measurementId: "G-0SEE00KPG7"
+};
+export default firebaseConfig
