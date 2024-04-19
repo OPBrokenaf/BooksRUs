c46cdea6ab6090b773d8b4eb24f79b7ea4f8eed6
nathan <159204967+DJ-Aizen@users.noreply.github.com>
2024-04-19 19:48:49 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvulO7FD2dwyV-1cAiaqf-Iq3SrbiO78k",
+  authDomain: "booksr-us-9f1e6.firebaseapp.com",
+  databaseURL: "https://booksr-us-9f1e6-default-rtdb.firebaseio.com",
+  projectId: "booksr-us-9f1e6",
+  storageBucket: "booksr-us-9f1e6.appspot.com",
+  messagingSenderId: "853405259540",
+  appId: "1:853405259540:web:57dad89954e22014661401",
+  measurementId: "G-0SEE00KPG7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', function(event){
+  event.preventDefault()
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  signInWithEmailAndPassword (auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("Logging in..")
+    window.location.href = "index.html";
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
+})
